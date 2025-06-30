b7b647a211f7e2fbfedd9191de91f0e9c84304d7
korizma desktop <dulekorizma55@gmail.com>
2025-06-30 12:38:48 +0200

    adding .gitignore

--- main.cpp
+++ main.cpp
@@ -8,22 +8,22 @@
 using namespace std;
 
 
-void handleSignal(int signal) {
-    if (signal == SIGTSTP) {
-        std::cout << "\nCtrl+D caught. Ignoring signal!" << std::endl;
-    }
-    if (signal == SIGSEGV)
-    {
-        std::cout << "\n oops!" << std::endl;
-        exit(0);
-    }
-}
+// void handleSignal(int signal) {
+//     if (signal == SIGTSTP) {
+//         std::cout << "\nCtrl+D caught. Ignoring signal!" << std::endl;
+//     }
+//     if (signal == SIGSEGV)
+//     {
+//         std::cout << "\n oops!" << std::endl;
+//         exit(0);
+//     }
+// }
 
 int main()
 {
-    signal(SIGINT, handleSignal);
-    signal(SIGTSTP, handleSignal);
-    signal(SIGSEGV, handleSignal);
+    // signal(SIGINT, handleSignal);
+    // signal(SIGTSTP, handleSignal);
+    // signal(SIGSEGV, handleSignal);
 
     cout << "Welcome to CLI!" << endl;
     Terminal::run();
